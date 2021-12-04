d813a882aaebc49ec6c6db2dda7aee34492e32d4
richwill28 <e0550368@u.nus.edu>
2021-12-05 04:34:45 +0800

    Feat(206): Add solution and readme

---
+++ reverse-linked-list/main.cpp
@@ -0,0 +1,40 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    // iterative solution
+    ListNode* reverseList(ListNode* head) {
+        ListNode* prev = nullptr;
+        ListNode* curr = head;
+        ListNode* next = nullptr;
+        while (curr != nullptr) {
+            next = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = next;
+        }
+        head = prev;
+        return head;
+    }
+
+    // recursive solution
+    ListNode* reverseList(ListNode* head) {
+        if (head == nullptr || head->next == nullptr) {
+            return head;
+        }
+        ListNode* curr = head;
+        ListNode* next = head->next;
+        head = reverseList(next);
+        curr->next = nullptr;
+        next->next = curr;
+        return head;
+    }
+};
