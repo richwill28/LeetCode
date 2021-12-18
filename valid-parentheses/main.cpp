a6e7dc9930aee6744bd62318c3591c1067e75db8
richwill28 <e0550368@u.nus.edu>
2021-12-18 18:00:13 +0800

    Fix(20): Add solution and readme

---
+++ valid-parentheses/main.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    // O(n) time and O(n) size complexity
+    bool isValid(string s) {
+        stack<char> st;
+        for (int i = 0; i < s.size(); i++) {
+            if (s[i] == '(' || s[i] == '{' || s[i] == '[') {
+                st.push(s[i]);
+            } else {
+                if (st.size() == 0) {
+                    return false;
+                }
+                char top = st.top();
+                st.pop();
+                if (s[i] == ')' && top != '(') {
+                    return false;
+                } else if (s[i] == '}' && top != '{') {
+                    return false;
+                } else if (s[i] == ']' && top != '[') {
+                    return false;
+                }
+            }
+        }
+
+        if (st.size() > 0) {
+            return false;
+        }
+        return true;
+    }
+};
